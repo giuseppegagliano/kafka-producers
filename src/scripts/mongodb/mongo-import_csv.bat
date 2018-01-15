f397dcdc83c26cb72284a78012d59b8490b85bee
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-01-15 23:41:23 +0100

    Errata corrige: renamed a file.

--- src/scripts/mongodb/mongo-import_csv.bat
+++
@@ -1,8 +0,0 @@
-TITLE MongoDB Import CSV
-SET "MONGO_HOME=C:\Program Files\MongoDB\Server\3.6"
-SET dataset=D:\machine_learning\datasets\iris\iris_with_head.csv
-SET collection=iris
-SET db=datasets
-
-"%MONGO_HOME%\bin\mongoimport.exe" -d %db% -c %collection% --type CSV --file %dataset% --headerline
-PAUSE
