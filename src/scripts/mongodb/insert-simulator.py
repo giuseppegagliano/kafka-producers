731309b9d5ecde9cbade479e66d40c7cfd01a15f
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-03-13 16:33:59 +0100

    Added configuration parameters

--- src/scripts/mongodb/insert-simulator.py
+++ src/scripts/mongodb/insert-simulator.py
@@ -11,6 +11,18 @@ parser.add_argument(
 	help="Source collection name."
 )
 parser.add_argument(
+	"--out",
+	type=str,
+	default="current",
+	help="Output collection name."
+)
+parser.add_argument(
+	"--db",
+	type=str,
+	default="datasets",
+	help="Source database name."
+)
+parser.add_argument(
 	"--timeout",
 	type=int,
 	default="3",
@@ -22,30 +34,28 @@ parser.add_argument(
 	default="3",
 	help="Number of lines to insert."
 )
+parser.add_argument(
+	"--server",
+	type=str,
+	default="localhost:27017",
+	help="Mongo server IP:PORT."
+)
 args, unparsed = parser.parse_known_args()
-
-
 system('title streamer')
-MONGO_SERV = 'localhost:27017'
-SRC_COLLECTION = args.src
-OUT_COLLECTION = 'current'
-TIMEOUT = args.timeout     # number of seconds
-BATCH_SIZE = args.batch
-
-client = MongoClient(MONGO_SERV)
-db = client.datasets
-srcColl = db[SRC_COLLECTION]
-outColl = db[OUT_COLLECTION]
+client = MongoClient(args.server)
+db = client[args.db]
+srcColl = db[args.src]
+outColl = db[args.out]
 outColl.delete_many({})
 
 docs = []
 for doc in srcColl.find():
-    if len(docs) == BATCH_SIZE:
+    if len(docs) == args.batch:
         for d in docs:
             print (d)
             outColl.insert_one(d)
         docs = []
-        time.sleep(TIMEOUT)
+        time.sleep(args.timeout)
     else:
         docs.append(doc)
         
