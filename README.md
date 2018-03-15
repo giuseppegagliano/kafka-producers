dc18b5019e6565a3803daef253a713d78bcd010e
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-03-15 02:22:03 +0100

    Added configuration parsing
    Added parser for the args to allow a flexible configuration, minor fixes
    on the README.

--- README.md
+++ README.md
@@ -3,10 +3,31 @@ This repository contains a set of Kafka producer to stream datasets for my Spark
 
 ## MongoDB Producer (MongoDB 3.6+)
 This producer watch a collection and streams whenever a new document is added (but you can extend for more operations).
+```
+$ java -jar kafka-mongo-producer.jar --help
+usage: MongoProducer [-h] [-t TOPIC] [-k KAFKA_SERVER] [-m MONGO_SERVER]
+                     [-d DATABASE] [-c COLLECTION]
+
+Watches a MongoDB collection and streams throughKafka its changes
+
+named arguments:
+  -h, --help             show this help message and exit
+  -t TOPIC, --topic TOPIC
+                         Specify the topic where to stream (default: milof)
+  -k KAFKA_SERVER, --kafka-server KAFKA_SERVER
+                         Specify the Kafka server (default: localhost:9092)
+  -m MONGO_SERVER, --mongo-server MONGO_SERVER
+                         Specify  the  Mongo  server  (default:  localhost:
+                         27017)
+  -d DATABASE, --database DATABASE
+                         Specify the database where  to find the collection
+                         (default: datasets)
+  -c COLLECTION, --collection COLLECTION
+                         Specify the collection to watch (default: current)
+```                     
 Steps:
 1. Install the tools you need (Kafka, MongoDB, Python and Java)
-2. Edit "mongo-import_csv.bat" as you need to load a dataset to MongoDB
-3. Run "kafka-servers-start.bat" (at the first run you need to remove EXIT to create the topic)
+3. Run "kafka-servers-start.bat"
 4. Run MongoDB in replica mode and initiate the replica
-5. Run the MongoProducer
+5. java -jar kafka-mongo-producer.jar
 6. Insert documents or run "insert-simulator.py"
--- src/main/java/it/gagliano/giuseppe/kafka/producer/MongoProducer.java
+++ src/main/java/it/gagliano/giuseppe/kafka/producer/MongoProducer.java
@@ -1,7 +1,10 @@
 package it.gagliano.giuseppe.kafka.producer;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.kafka.clients.admin.Config;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -15,22 +18,34 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public class MongoProducer {
 
 	private static KafkaProducer<Integer, String> producer = null;
-	// private static final Boolean isAsync = true;
 	private static int messageNo = 1;
 	private static String messageStr = "";
 	private static long startTime = System.currentTimeMillis();
+	public static Map<String, String> conf = new HashMap<String, String>(){{
+		put("topic","milof");
+		put("mongo-server","localhost:27017");
+		put("kafka-server","localhost:9092");
+		put("database","datasets");
+		put("collection","current");
+		}};
 
 	public static void main(String[] args) {
-
+        parseArgs(args);
+        
 		Block<ChangeStreamDocument<Document>> printBlock = new Block<ChangeStreamDocument<Document>>() {
 			@Override
 			public void apply(final ChangeStreamDocument<Document> changeStreamDocument) {
 				try {
 					messageStr = changeStreamDocument.getFullDocument().toJson();
-					producer.send(new ProducerRecord<>(KafkaProducerParams.TOPIC, messageNo, messageStr),
+					producer.send(new ProducerRecord<>(conf.get("topic"), messageNo, messageStr),
 							new DemoCallBack(startTime, messageNo, messageStr));
 
 					System.out.println("Sent " + changeStreamDocument);
@@ -39,34 +54,60 @@ public class MongoProducer {
 			}
 		};
 		producer = createProducer();
-		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://" + KafkaProducerParams.MONGO_SERVER));
-		MongoDatabase database = mongoClient.getDatabase(KafkaProducerParams.DATABASE);
-		MongoCollection<Document> collection = database.getCollection(KafkaProducerParams.COLLECTION);
+		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://" + conf.get("mongo-server")));
+		MongoDatabase database = mongoClient.getDatabase(conf.get("database"));
+		MongoCollection<Document> collection = database.getCollection(conf.get("collection"));
 		collection.watch().forEach(printBlock);
 		mongoClient.close();
 	}
 
+	public static void parseArgs(String[] args) {
+		ArgumentParser parser = ArgumentParsers.newFor("MongoProducer").build()
+                .defaultHelp(true)
+                .description("Watches a MongoDB collection and streams through "
+                		+ "Kafka its changes");
+        parser.addArgument("-t", "--topic")
+                .setDefault("milof")
+                .help("Specify the topic where to stream");
+        parser.addArgument("-k", "--kafka-server")
+		        .setDefault("localhost:9092")
+		        .help("Specify the Kafka server");
+        parser.addArgument("-m", "--mongo-server")
+		        .setDefault("localhost:27017")
+		        .help("Specify the Mongo server");
+        parser.addArgument("-d", "--database")
+		        .setDefault("datasets")
+		        .help("Specify the database where to find the collection");
+        parser.addArgument("-c", "--collection")
+		        .setDefault("current")
+		        .help("Specify the collection to watch");
+    	Namespace ns = null;
+        try {
+            ns = parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+        conf.put("collection", ns.getString("collection"));
+        conf.put("database", ns.getString("database"));
+        conf.put("kafka-server", ns.getString("kafka_server"));
+        conf.put("mongo-server", ns.getString("mongo_server"));
+        conf.put("topic", ns.getString("topic"));
+	}
+	
 	public static KafkaProducer<Integer, String> createProducer() {
 		Properties properties = new Properties();
-		properties.put("bootstrap.servers", KafkaProducerParams.KAFKA_SERVER);
-		// properties.put("client.id", CLIENT_ID);
+		properties.put("bootstrap.servers", conf.get("kafka-server"));
 		properties.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
 		properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		return new KafkaProducer<>(properties);
 	}
+	
 }
 
 class DemoCallBack implements Callback {
 
-//	private final long startTime;
-//	private final int key;
-//	private final String message;
-
-	public DemoCallBack(long startTime, int key, String message) {
-//		this.startTime = startTime;
-//		this.key = key;
-//		this.message = message;
-	}
+	public DemoCallBack(long startTime, int key, String message) {}
 
 	/**
 	 * onCompletion method will be called when the record sent to the Kafka Server
@@ -80,15 +121,6 @@ class DemoCallBack implements Callback {
 	 *            error occurred.
 	 */
 	public void onCompletion(RecordMetadata metadata, Exception exception) {
-//		long elapsedTime = System.currentTimeMillis() - startTime;
-		if (metadata != null) {
-			// System.out.println(
-			// "message(" + key + ", " + message + ") sent to partition(" +
-			// metadata.partition() +
-			// "), " +
-			// "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
-		} else {
-			exception.printStackTrace();
-		}
+		if (metadata == null)	exception.printStackTrace();
 	}
 }
