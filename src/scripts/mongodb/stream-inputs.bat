b89bdb8d4b3a16502fec881ff901747cc0d309d6
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-05-16 11:12:00 +0200

    Added openstack deploy files

--- src/scripts/mongodb/stream-inputs.bat
+++ src/scripts/mongodb/stream-inputs.bat
@@ -1,5 +1,5 @@
 MODE con: cols=80 lines=25
 TITLE MongoDB Server
-python insert-simulator.py --src pendigits_full_with_head_noise5_train --timeout 5 --batch 5
+python insert-simulator.py --src pendigits_full_with_head_noise5_test --timeout 6 --batch 3
 EXIT
 python insert-simulator.py --src pendigits_full_with_head_noise5_test --timeout 0 --batch 1
