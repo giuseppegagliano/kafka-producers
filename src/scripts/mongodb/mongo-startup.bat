a28ac9c1545a0e7a2b1675609f9f4cf35caa44b9
Giuseppe Gagliano <gagliano.giuseppe.antonio@gmail.com>
2018-05-17 15:14:28 +0200

    minor change

--- src/scripts/mongodb/mongo-startup.bat
+++ src/scripts/mongodb/mongo-startup.bat
@@ -1,4 +1,4 @@
 MODE con: cols=80 lines=25
 TITLE MongoDB Server
-"C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --port 27017 --dbpath C:\mongodb --replSet rs0 --bind_ip localhost,localhost --enableMajorityReadConcern
+C:\Progra~1\MongoDB\Server\3.6\bin\mongod.exe --port 27017 --dbpath C:\mongodb --replSet rs0 --bind_ip localhost,localhost --enableMajorityReadConcern
 PAUSE
